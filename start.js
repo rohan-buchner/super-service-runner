2343a0f64131dfd322fae070c0b7a1bcba73eeb1
Rohan Büchner <rohan.buchner@gmail.com>
2017-06-06 10:17:39 +0200

    WIP Cron not working :(

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 const autoLoader = require('auto-loader');
 const readYaml = require('read-yaml');
+const CronJob = require('cron').CronJob;
 
 var services = autoLoader.load(__dirname +'/services')
 
@@ -8,6 +9,22 @@ readYaml('config.yml', function(err, conf) {
 	if (err) throw err;
   	
  	Object.keys(conf).forEach(function (key) {
-   		services[key](conf[key])
+
+ 		console.log('Creating CRON for ' + key);
+ 		
+ 		new CronJob(conf[key].cron,
+ 			// start hook
+ 			function(data) {
+				console.log('start', data);
+   				services[key](conf[key]);
+			},
+			// end hook 
+			function(data) {
+				console.log('done', data)
+			}, 
+			// start now
+			true, 
+			// timezone
+			'America/Los_Angeles');  //TODO: get correct timesone
 	});	
 });
