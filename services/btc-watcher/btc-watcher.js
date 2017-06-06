fb26ef477641c141f0a11544975a708b1b28f276
Rohan Büchner <rohan.buchner@gmail.com>
2017-06-06 19:08:15 +0200

    wip

--- services/btc-watcher/btc-watcher.js
+++ services/btc-watcher/btc-watcher.js
@@ -1,12 +1,30 @@
 const rp = require('request-promise');
 
 // https://api.mybitx.com/api/1/ticker?pair=XBTZAR
-function getPairRate(pair) {
+function bitxTicker() {
 
 	var options = {
 	    uri: 'https://api.mybitx.com/api/1/ticker',
 	    qs: {
-	        pair: pair // -> uri + '?access_token=xxxxx%20xxxxx' 
+	        pair: 'XBTZAR' // -> uri + '?access_token=xxxxx%20xxxxx' 
+	    },
+	    headers: {
+	        'User-Agent': 'Request-Promise'
+	    },
+	    json: true // Automatically parses the JSON string in the response 
+	};
+ 
+	return rp(options);  
+};
+
+//https://www.cryptocompare.com/api/#-api-data-price-
+function cryptocompare() {
+
+	var options = {
+	    uri: 'https://min-api.cryptocompare.com/data/pricemulti',
+	    qs: {
+	        fsyms: 'ETH,DASH,BTC,LTC,DOGE',
+	        tsyms: 'USD'
 	    },
 	    headers: {
 	        'User-Agent': 'Request-Promise'
@@ -18,7 +36,14 @@ function getPairRate(pair) {
 };
 
 module.exports = function (config) {
-	getPairRate(config.rate)
+
+	bitxTicker()
+    	.then(data => {
+        	//slack.publish(data)
+        	console.log(data)
+      	});
+
+	cryptocompare()
     	.then(data => {
         	//slack.publish(data)
         	console.log(data)
